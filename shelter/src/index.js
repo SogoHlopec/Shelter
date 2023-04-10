0f9c698a4b4ed867078867d4dce3e89b45895308
SogoHlopec <sogo246@gmail.com>
2023-04-11 01:38:53 +0300

    feat: add alert

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -2,6 +2,4 @@ import "./index.html";
 import "./style.scss";
 import "./modules/burger";
 import "./modules/popap"
-console.log(`
-Все пункты выполнены. Балл 100/100.
-`);
+alert("Пожалуйста, проверьте работу в последний день. Я ещё доделаю функционал")
