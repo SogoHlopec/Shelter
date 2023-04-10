fa683d4010665556653d0f5cb963f927807f9566
SogoHlopec <sogo246@gmail.com>
2023-04-11 02:20:50 +0300

    feat: add adaptive for popup

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -2,4 +2,4 @@ import "./index.html";
 import "./style.scss";
 import "./modules/burger";
 import "./modules/popap"
-alert("Пожалуйста, проверьте работу в последний день. Я ещё доделаю функционал")
+// alert("Пожалуйста, проверьте работу в последний день. Я ещё доделаю функционал")
